63be073ac595dbdd4132120c1cf3b6f0da0690be
martia_f <44898501+MinecraftPublisher@users.noreply.github.com>
2022-02-16 15:09:22 +0330

    Update drag_drop_dlc.js

--- drag_drop_dlc.js
+++ drag_drop_dlc.js
@@ -1,4 +1,3 @@
 function dd_0() {
-  div.innerHTML = "the drag-drop dlc is not available yet<br><br>\
-  <a href='#' onclick=\"stage('main', 'default');\">back to main menu<a/>";
+  div.innerHTML = "the drag-drop dlc is not available yet";
 }
