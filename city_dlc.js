a050bc4a8a7576af6e5ec599f20c849125ec306e
martia_f <44898501+MinecraftPublisher@users.noreply.github.com>
2022-02-16 15:08:51 +0330

    Update city_dlc.js

--- city_dlc.js
+++ city_dlc.js
@@ -1,4 +1,3 @@
 function city_0() {
-  div.innerHTML = "the city dlc is not available yet<br><br>\
-  <a href='#' onclick=\"stage('main', 'default');\">back to main menu<a/>";
+  div.innerHTML = "the city dlc is not available yet";
 }
