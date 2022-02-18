c5bf46679cbd1a656174a8490d99704a9a5c2d33
martia_f <44898501+MinecraftPublisher@users.noreply.github.com>
2022-02-18 21:41:09 +0330

    make sure to pick up the keycard in awasp, it's a one-time thing!

--- basement_dlc.js
+++ basement_dlc.js
@@ -1,3 +1,3 @@
 function base_0 () {
-  div.innerHTML = "the basement dlc is not available yet"
+  div.innerHTML = "the lights are off down there, and rats are hanging out near the entrance. you see a keycard reader but it's off."
 }
