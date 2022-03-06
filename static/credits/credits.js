70b408213aeeb40c19023bade1c38a55684f630a
martia_f <mforoud86@gmail.com>
2022-03-06 22:26:23 +0330

    Added credits...

---
+++ static/credits/credits.js
@@ -0,0 +1,16 @@
+function main() {
+    div.innerHTML = `<h1>Credits</h1>
+<ul>
+    <li>martia_f (MinecraftPublisher) - Main Designer</li>
+    <li>nobody else, it's a personal project!</li>
+</ul>
+
+<h4>Additional Credits</h4>
+<ul>
+    <li><a href="https://www.youtube.com/channel/UCiSKnkKCKAQVxMUWpZQobuQ">Myuu</a> for the theme song, Wasted Wonderland</li>
+    <li><a href="https://fonts.google.com/">Google Fonts</a> for the Amatic SC font</li>
+    <li><a href="https://rolandhuse.com/">Roland Huse</a> for the WhirlyWood font (aka the more readable option)</li>
+    <li><a href="https://www.jetbrains.com/">Jetbrains</a> for their awesome products</li>
+    <li><a href="https://github.com/">GitHub</a> for Copilot and their amazing service</li>
+</ul>`;
+}
