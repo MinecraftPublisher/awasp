df164735c0acd4ac99306f07f5c879abd7958b44
martia_f <mforoud86@gmail.com>
2022-03-06 22:22:09 +0330

    Categorized files into awasp.js and stage2.js

--- static/awasp/awasp.js
+++ static/awasp/awasp.js
@@ -44,26 +44,3 @@ function stage1_bedroom_bed() {
     " + (get('knife_acquired') === 'true' ? "" : "<a href=\"#\" onclick=\"set('knife_acquired', 'true'); stage('awasp', 'stage1');\">pick it up, I'll need it</a><br>") + "\
     <a href=\"#\" onclick=\"stage('awasp', 'stage1_bedroom');\">back to the bedroom</a>";
 }
-
-function stage2() {
-    div.innerHTML = "you are in the living room, it has a ton of chairs inside, a door to the bedroom and a TV that's flickering. you wonder what to do.<br><br>\
-    <a href=\"#\" onclick=\"stage('awasp', 'stage1_bedroom');\">go to the bedroom</a><br>\
-    <href =\"#\" onclick=\"stage('awasp', 'stage2_tv');\">examine the TV</a><br>\
-    <a href=\"#\" onclick=\"stage('awasp', 'stage1');\">go to the entrance</a>";
-}
-
-function stage2_tv() {
-    div.innerHTML = "you look behind the TV, ";
-    if (get('bug_acquired') === 'true') {
-        div.innerHTML += "and don't find anything. you could feel the weight of that bug device thing in your pocket.<br><br>";
-    } else {
-        div.innerHTML += "and you see a huge robotic insect connected to the back of the TV, ";
-        if (get('knife_acquired') === 'true') {
-            div.innerHTML += "you pull out your knife, ready to disconnect it.<br><br>\
-            <a href=\"#\" onclick=\"set('bug_acquired', 'true'); stage('awasp', 'stage2_tv');\">remove it and put it in your pocket</a>";
-        } else {
-            div.innerHTML += "but there isn't anything you could do about it, it's stuck very hard and only a knife would separate it.<br><br>";
-        }
-    }
-    div.innerHTML += "<a href=\"#\" onclick=\"stage('awasp', 'stage2');\">return to the living room</a>"
-}
